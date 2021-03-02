be1849c1d689a58ddaf842b082ad8306f9827cc5
blQSheep <camel327@web.de>
2021-03-02 02:22:36 +0100

    added possibility to visit different pages
    user can now choose between two different publishers

--- headlines.py
+++ headlines.py
@@ -9,19 +9,26 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 
-BBC_Feed = "http://feeds.bbci.co.uk/news/rss.xml"
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_Feed)
+@app.route("/bbc")
+def bbc():
+    return get_news('bbc')
+
+@app.route("/cnn")
+def cnn():
+    return get_news('cnn')
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return """<html>
     <body>
-        <h1> BBC Headlines </h1>
+        <h1>Headlines </h1>
         <b>{0}</b> <br/>
         <i>{1}</i> <br/>
         <p>{2}</p> <br/>
     </body>
     </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
-
+    
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
