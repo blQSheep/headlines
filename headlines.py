838bd0a9173be1166eb8679fd75513aab76191e7
blQSheep <camel327@web.de>
2021-03-04 00:32:34 +0100

    added template html file
    file now reads html from a file instead of python string

--- headlines.py
+++ headlines.py
@@ -1,7 +1,10 @@
 import feedparser
 from flask import Flask
+from flask import render_template
+import views
 
 app = Flask(__name__)
+
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn':  'http://rss.cnn.com/rss/edition.rss',
@@ -9,22 +12,16 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 
+
 @app.route("/")
 @app.route("/<publication>")
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-    <body>
-        <h1>Headlines </h1>
-        <b>{0}</b> <br/>
-        <i>{1}</i> <br/>
-        <p>{2}</p> <br/>
-    </body>
-    </html>""".format(first_article.get("title"),
-                      first_article.get("published"),
-                      first_article.get("summary"))
+    return render_template("home.html")
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(port=5000, debug=True) #host='0.0.0.0', 
+ 
